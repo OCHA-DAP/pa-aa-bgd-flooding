ba80d3cd952740a6dd1fcfb1cbf7eafe7847b1b6
Pauline Wairimu <paunimo@gmail.com>
2023-06-14 14:25:21 +0300

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,6 @@
 from datetime import date
 from pathlib import Path
-import geopandas as gpd
+
 import os
 from ochanticipy import CodAB, GeoBoundingBox, create_custom_country_config
 
@@ -12,7 +12,6 @@ leadtime_max = 15
 
 codab = CodAB(country_config=country_config)
 admin0 = codab.load(admin_level=0)
-
 geo_bounding_box = GeoBoundingBox.from_shape(admin0)
 
 reanalysis_end_date = date(2022, 12, 31)
